15901db91012d118405fba93454654a84d12c320
Jozefu25 <youssef.harrabi@esen.tn>
2024-02-11 14:53:38 +0100

    initial commit

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+import utiles.MyConnections;
+import entities.Utilisateurs;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyConnections mc= new MyConnections();
+
+
+    }
+}
